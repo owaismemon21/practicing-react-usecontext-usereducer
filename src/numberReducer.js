000bdb7fc4c280199f5fa2a73b6dfd56d7889735
Owais Memon <owaismemon21@gmail.com>
2020-11-26 21:43:20 +0500

    fix issue in number reducer.

--- src/numberReducer.js
+++ src/numberReducer.js
@@ -1,10 +1,10 @@
 const NumberReducer = (state, action) => {
-    switch(action.type) {
+    switch (action.type) {
         case 'INCREMENT':
             return state + 1
         case 'DECREMENT':
             return state - 1
     }
 }
-
+  
 export default NumberReducer;
